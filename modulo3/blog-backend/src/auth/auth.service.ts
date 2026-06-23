154818f7c11e9de6ac2106b343907e223d1b004f
Anahi-droid <lozaanahi02@gmail.com>
2026-06-23 03:39:15 -0500

    CLase 23-05-2026

--- modulo3/blog-backend/src/auth/auth.service.ts
+++ modulo3/blog-backend/src/auth/auth.service.ts
@@ -6,34 +6,42 @@ import { CreateUserDto } from '../users/dto/create-user.dto';
 import * as bcrypt from 'bcrypt';
 import { User } from 'src/users/users.entity';
 
+
 @Injectable()
 export class AuthService {
-  constructor(
-    private readonly usersService: UsersService,
-    private readonly jwtService: JwtService,
-  ) {}
+ constructor(
+   private readonly usersService: UsersService,
+   private readonly jwtService: JwtService,
+ ) {}
 
-  async login(loginDto: LoginDto): Promise<string | null> {
-    try {
-      const user: User | null = await this.usersService.findByUsername(loginDto.username!);
-      if (!user) return null;
 
-      const isValid = await bcrypt.compare(loginDto.password!, user.password!);
-      if (!isValid) return null;
+ async login(loginDto: LoginDto): Promise<string | null> {
+   try {
+     const user: User | null = await this.usersService.findByUsername(loginDto!.username!);
+     if (!user) return null;
 
-      const payload = { id: user.id, username: user.username };
-      return this.jwtService.sign(payload);
-    } catch (err) {
-      console.error('Unexpected login error:', err);
-      return null;
-    }
-  }
 
-  async register(createUserDto: CreateUserDto): Promise<string | null> {
-    const user = await this.usersService.create(createUserDto);
-    if (!user) return null;
+     const isValid = await bcrypt.compare(loginDto!.password!, user!.password!);
+     if (!isValid) return null;
 
-    const payload = { id: user.id, email: user.username };
-    return this.jwtService.sign(payload);
-  }
+
+     const payload = { id: user.id, username: user.username };
+     return this.jwtService.sign(payload);
+   } catch (err) {
+     console.error('Unexpected login error:', err);
+     return null;
+   }
+ }
+
+
+ async register(createUserDto: CreateUserDto): Promise<string | null> {
+   const user = await this.usersService.create(createUserDto);
+   if (!user) return null;
+
+
+   const payload = { id: user.id, email: user.username };
+   return this.jwtService.sign(payload);
+ }
 }
+
+
